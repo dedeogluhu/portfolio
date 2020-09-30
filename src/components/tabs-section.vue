2a911cf3ee37db0d6e5e2eb00bce98c2ab3498f0
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-10-01 00:12:33 +0300

    made tabs seperate components

--- src/components/tabs-section.vue
+++ src/components/tabs-section.vue
@@ -3,13 +3,13 @@
     <b-card no-body>
       <b-tabs card fill>
         <b-tab title="Skills" active>
-          <b-card-text>Skills.....</b-card-text>
+          <b-card-text> <skillsTab /> </b-card-text>
         </b-tab>
         <b-tab title="Projects">
-          <b-card-text>Projects.....</b-card-text>
+          <b-card-text> <projectsTab /> </b-card-text>
         </b-tab>
         <b-tab title="Resume">
-          <b-card-text>Resume.......</b-card-text>
+          <b-card-text> <resumeTab /> </b-card-text>
         </b-tab>
       </b-tabs>
     </b-card>
@@ -17,8 +17,17 @@
 </template>
 
 <script>
+import skillsTab from "./tab-contents/skills-tab";
+import projectsTab from "./tab-contents/projects-tab";
+import resumeTab from "./tab-contents/resume-tab";
+
 export default {
   name: "tabs-section",
+  components: {
+    skillsTab,
+    projectsTab,
+    resumeTab,
+  },
 };
 </script>
 
