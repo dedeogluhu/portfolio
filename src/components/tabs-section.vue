ccce2419252a7a0f853672592906aca8f546274c
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-09-30 22:38:53 +0300

    added tabs section

---
+++ src/components/tabs-section.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container mt-5">
+    <b-card no-body>
+      <b-tabs card fill>
+        <b-tab title="Skills" active>
+          <b-card-text>Skills.....</b-card-text>
+        </b-tab>
+        <b-tab title="Projects">
+          <b-card-text>Projects.....</b-card-text>
+        </b-tab>
+        <b-tab title="Resume">
+          <b-card-text>Resume.......</b-card-text>
+        </b-tab>
+      </b-tabs>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tabs-section",
+};
+</script>
+
+<style>
+</style>
