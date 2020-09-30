4e9b104ea8bd4f3b20062e8c1636faabfe5ea7ba
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-09-30 23:04:47 +0300

    desgn main-info

--- src/components/main-info.vue
+++ src/components/main-info.vue
@@ -1,12 +1,40 @@
 <template>
   <div class="container">
     <div class="card">
-      <h5 class="card-header">Hüseyin Dedeoğlu</h5>
+      <h4 class="card-header">Hüseyin Dedeoğlu</h4>
       <div class="card-body">
-        <h5 class="card-title">Hello There</h5>
+        <h4 class="card-title">Hello There</h4>
         <p class="card-text">
           I'm a passionate fullstack developer who loves to build web stuff.
         </p>
+        <p class="card-text">You can reach me here:</p>
+        <a
+          href="mailto:huseyin_dedeoglu1@outlook.com"
+          class="btn btn-primary m-3"
+        >
+          <font-awesome-icon
+            :icon="['fas', 'envelope']"
+            size="lg"
+            class="icon-size"
+          />
+        </a>
+        <a href="https://github.com/dedeogluhu" class="btn btn-secondary m-3">
+          <font-awesome-icon
+            :icon="['fab', 'github']"
+            size="lg"
+            class="icon-size"
+          />
+        </a>
+        <a
+          href="https://linkedin.com/in/dedeogluhu"
+          class="btn btn-primary m-3"
+        >
+          <font-awesome-icon
+            :icon="['fab', 'linkedin']"
+            size="lg"
+            class="icon-size"
+          />
+        </a>
       </div>
     </div>
   </div>
@@ -18,5 +46,8 @@ export default {
 };
 </script>
   
-  <style>
+<style scoped>
+.icon-size {
+  font-size: 40pt;
+}
 </style>
