c75eeda14c1c1875a57e4d9e30eacbfd06908888
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-09-30 22:38:42 +0300

    added header section

---
+++ src/components/main-info.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <h5 class="card-header">Hüseyin Dedeoğlu</h5>
+      <div class="card-body">
+        <h5 class="card-title">Hello There</h5>
+        <p class="card-text">
+          I'm a passionate fullstack developer who loves to build web stuff.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+  
+  <script>
+export default {
+  name: "main-info",
+};
+</script>
+  
+  <style>
+</style>
