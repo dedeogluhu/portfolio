4812787cf832faaae465ee5bf1fe4c6deab5a67f
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-09-30 22:42:57 +0300

    added and imported font awsome

--- src/main.js
+++ src/main.js
@@ -10,6 +10,15 @@ Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 
+//Font Awsome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 
 
 Vue.config.productionTip = false
