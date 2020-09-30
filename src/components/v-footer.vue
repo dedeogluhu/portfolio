316769d03bfaddc3ee2181275ce3125a93b2ebe4
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-09-30 22:38:59 +0300

    added footer

---
+++ src/components/v-footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="container text-center mt-5">
+    <h6>Copyright &copy; Hüseyin Dedeoğlu 2020</h6>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
