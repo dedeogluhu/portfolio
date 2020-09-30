21cb8c058c97b81213fc9fce076f39989bf6c699
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-10-01 00:17:25 +0300

    added dummy content to skills tab

--- src/components/tab-contents/skills-tab.vue
+++ src/components/tab-contents/skills-tab.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>Skills...</div>
+  <div class="container">
+    <h3>Frontend:</h3>
+    <p>Html,Css,Javascript,Vue</p>
+    <h3>Backend:</h3>
+    <p>Dotnet, Python, Javascript</p>
+    <h3>Other Skills:</h3>
+    <p>Version Control, Django, Mssql</p>
+  </div>
 </template>
 
 <script>
