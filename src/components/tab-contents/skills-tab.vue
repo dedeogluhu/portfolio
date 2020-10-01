b58522ad4e6b79f1689595436057e0fbe07f7a2a
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-10-01 17:48:09 +0300

    changed skills table styling

--- src/components/tab-contents/skills-tab.vue
+++ src/components/tab-contents/skills-tab.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col">
         <ul class="list-group list-group-flush">
-          <li class="list-group-item list-group-item-dark">Frontend</li>
+          <li class="list-group-item font-weight-bold">Frontend</li>
           <li class="list-group-item">Javascript</li>
           <li class="list-group-item">Vuejs</li>
           <li class="list-group-item">Html</li>
@@ -12,7 +12,7 @@
       </div>
       <div class="col">
         <ul class="list-group list-group-flush">
-          <li class="list-group-item list-group-item-dark">Backend</li>
+          <li class="list-group-item font-weight-bold">Backend</li>
           <li class="list-group-item">Javascript</li>
           <li class="list-group-item">Python</li>
           <li class="list-group-item">.net</li>
@@ -20,7 +20,7 @@
       </div>
       <div class="col">
         <ul class="list-group list-group-flush">
-          <li class="list-group-item list-group-item-dark">Other</li>
+          <li class="list-group-item font-weight-bold">Other</li>
           <li class="list-group-item">Version Control</li>
           <li class="list-group-item">MSSQL</li>
           <li class="list-group-item">Linux</li>
@@ -28,7 +28,7 @@
       </div>
       <div class="col">
         <ul class="list-group list-group-flush">
-          <li class="list-group-item list-group-item-dark">Languages</li>
+          <li class="list-group-item font-weight-bold">Languages</li>
           <li class="list-group-item">Turkish(Native)</li>
           <li class="list-group-item">English(Advanced)</li>
           <li class="list-group-item">Chinese(Beginner)</li>
