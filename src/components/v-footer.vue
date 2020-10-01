222964c2b9759abc3c96abacba17635600c4dda7
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-10-01 13:45:09 +0300

    added padding to footer

--- src/components/v-footer.vue
+++ src/components/v-footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="container text-center mt-5">
+  <footer class="container text-center mt-5 py-2">
     <h6>Copyright &copy; Hüseyin Dedeoğlu 2020</h6>
   </footer>
 </template>
