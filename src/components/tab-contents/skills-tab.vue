c284705a9d02e26758b7b6e5d4ae2df0b5dbe84f
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-10-01 15:31:59 +0300

    added skills tab content

--- src/components/tab-contents/skills-tab.vue
+++ src/components/tab-contents/skills-tab.vue
@@ -1,11 +1,41 @@
 <template>
   <div class="container">
-    <h3>Frontend:</h3>
-    <p>Html,Css,Javascript,Vue</p>
-    <h3>Backend:</h3>
-    <p>Dotnet, Python, Javascript</p>
-    <h3>Other Skills:</h3>
-    <p>Version Control, Django, Mssql</p>
+    <div class="row">
+      <div class="col">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item list-group-item-dark">Frontend</li>
+          <li class="list-group-item">Javascript</li>
+          <li class="list-group-item">Vuejs</li>
+          <li class="list-group-item">Html</li>
+          <li class="list-group-item">Css</li>
+        </ul>
+      </div>
+      <div class="col">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item list-group-item-dark">Backend</li>
+          <li class="list-group-item">Javascript</li>
+          <li class="list-group-item">Python</li>
+          <li class="list-group-item">.net</li>
+        </ul>
+      </div>
+      <div class="col">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item list-group-item-dark">Other</li>
+          <li class="list-group-item">Version Control</li>
+          <li class="list-group-item">MSSQL</li>
+          <li class="list-group-item">Linux</li>
+        </ul>
+      </div>
+      <div class="col">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item list-group-item-dark">Languages</li>
+          <li class="list-group-item">Turkish(Native)</li>
+          <li class="list-group-item">English(Advanced)</li>
+          <li class="list-group-item">Chinese(Beginner)</li>
+          <li class="list-group-item">German(Beginner)</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
