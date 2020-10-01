48fe093de40d66ebae554279be22bf28e7b74211
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-10-01 18:29:22 +0300

    imported dummy resume image

--- src/components/tab-contents/resume-tab.vue
+++ src/components/tab-contents/resume-tab.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="container">Resume....</div>
+  <div class="container">
+    <div class="alert alert-danger">
+      This is just a test resume it is not real
+    </div>
+    <img src="../../assets/dummy-resume.jpg" alt="Resume" width="1050" />
+  </div>
 </template>
 
 <script>
@@ -9,4 +14,7 @@ export default {
 </script>
 
 <style scoped>
+.resume-image {
+  background-image: url("../../assets/dummy-resume.jpg");
+}
 </style>
