b5b56b09ef6c686514fb43ee85341e5d4eab35ef
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-10-02 15:39:57 +0300

    made header styling

--- src/components/main-info.vue
+++ src/components/main-info.vue
@@ -3,38 +3,45 @@
     <div class="card">
       <h4 class="card-header">Hüseyin Dedeoğlu</h4>
       <div class="card-body">
-        <h5 class="card-title">Hi There <a href="#" class="cursor">👋</a></h5>
-        <h5 class="card-text">
-          I'm a passionate web developer who loves to build stuff.
+        <h5 class="row">
+          <div class="col">
+            Hi There <a href="#" class="cursor">👋</a>
+
+            <h5 class="card-text">
+              <br />
+              I'm a passionate web developer who loves to build stuff.
+            </h5>
+          </div>
+          <div class="col text-right">
+            <a
+              href="mailto:huseyin_dedeoglu1@outlook.com"
+              class="btn btn-primary m-2"
+            >
+              <font-awesome-icon
+                :icon="['fas', 'envelope']"
+                size="lg"
+                class="icon-size"
+              />
+            </a>
+            <a href="https://github.com/dedeogluhu" class="btn btn-dark m-2">
+              <font-awesome-icon
+                :icon="['fab', 'github']"
+                size="lg"
+                class="icon-size"
+              />
+            </a>
+            <a
+              href="https://linkedin.com/in/dedeogluhu"
+              class="btn btn-primary m-2"
+            >
+              <font-awesome-icon
+                :icon="['fab', 'linkedin']"
+                size="lg"
+                class="icon-size"
+              />
+            </a>
+          </div>
         </h5>
-        <p class="card-text">You can reach me here:</p>
-        <a
-          href="mailto:huseyin_dedeoglu1@outlook.com"
-          class="btn btn-primary m-3"
-        >
-          <font-awesome-icon
-            :icon="['fas', 'envelope']"
-            size="lg"
-            class="icon-size"
-          />
-        </a>
-        <a href="https://github.com/dedeogluhu" class="btn btn-dark m-3">
-          <font-awesome-icon
-            :icon="['fab', 'github']"
-            size="lg"
-            class="icon-size"
-          />
-        </a>
-        <a
-          href="https://linkedin.com/in/dedeogluhu"
-          class="btn btn-primary m-3"
-        >
-          <font-awesome-icon
-            :icon="['fab', 'linkedin']"
-            size="lg"
-            class="icon-size"
-          />
-        </a>
       </div>
     </div>
   </div>
