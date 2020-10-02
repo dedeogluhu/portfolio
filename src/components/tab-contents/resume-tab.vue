a8c2f638987d19ade65e1fc3d1790d73bae26b99
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-10-02 15:40:47 +0300

    added responsibility to resume image

--- src/components/tab-contents/resume-tab.vue
+++ src/components/tab-contents/resume-tab.vue
@@ -3,7 +3,7 @@
     <div class="alert alert-danger">
       This is just a test resume it is not real
     </div>
-    <img src="../../assets/dummy-resume.jpg" alt="Resume" width="1050" />
+    <img src="../../assets/dummy-resume.jpg" alt="Resume" />
   </div>
 </template>
 
@@ -16,5 +16,9 @@ export default {
 <style scoped>
 .resume-image {
   background-image: url("../../assets/dummy-resume.jpg");
+}
+img {
+  width: 100%;
+  height: auto;
 }
 </style>
