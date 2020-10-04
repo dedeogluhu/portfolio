d48ea80068eae01ed38a0a6bc2ed0906750253d3
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-10-04 16:41:22 +0300

    change footer color

--- src/components/v-footer.vue
+++ src/components/v-footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="container text-center mt-2 py-2">
-    <h6>Copyright &copy; 2020 Hüseyin Dedeoğlu</h6>
+    <h6 class="text-white">Copyright &copy; 2020 Hüseyin Dedeoğlu</h6>
   </footer>
 </template>
 
