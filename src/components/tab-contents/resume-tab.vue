86b33b698ad3225dab7e91d3b57c745e7986b528
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-10-04 18:41:04 +0300

    Small info changings

--- src/components/tab-contents/resume-tab.vue
+++ src/components/tab-contents/resume-tab.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <b-alert show dismissible fade variant="danger"
-      >This is just a test resume it is not real
+      >This is just a test resume. I am going to add the real one soon.
     </b-alert>
     <img src="../../assets/dummy-resume.jpg" alt="Resume" />
   </div>
