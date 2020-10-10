3279abca7cbd4bbc36bd9665baf5c0f74ac2fe91
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-10-10 15:29:25 +0300

    Update Skill Tab

--- src/components/tab-contents/skills-tab.vue
+++ src/components/tab-contents/skills-tab.vue
@@ -3,10 +3,10 @@
     <div class="row">
       <div class="col">
         <ul class="list-group list-group-flush">
-          <li class="list-group-item font-weight-bold bg-light">Frontend</li>
+          <li class="list-group-item font-weight-bold bg-light">Languages</li>
           <li
             class="list-group-item bg-light"
-            v-for="element in frontend"
+            v-for="element in programmingLanguages"
             :key="element"
           >
             {{ element }}
@@ -15,10 +15,10 @@
       </div>
       <div class="col">
         <ul class="list-group list-group-flush">
-          <li class="list-group-item font-weight-bold bg-light">Backend</li>
+          <li class="list-group-item font-weight-bold bg-light">Frameworks</li>
           <li
             class="list-group-item bg-light"
-            v-for="element in backend"
+            v-for="element in frameworks"
             :key="element"
           >
             {{ element }}
@@ -39,7 +39,7 @@
       </div>
       <div class="col">
         <ul class="list-group list-group-flush bg-light">
-          <li class="list-group-item font-weight-bold bg-light">Languages</li>
+          <li class="list-group-item font-weight-bold bg-light">Speaking</li>
           <li
             class="list-group-item bg-light"
             v-for="element in languages"
@@ -58,9 +58,9 @@ export default {
   name: "skills-tab",
   data: function () {
     return {
-      frontend: ["Javascript", "Vuejs", "Html", "Css"],
-      backend: ["Javascript", "Python", "C#"],
-      other: ["Desktop apps with C#", "Version Control", "MSSQL", "Linux"],
+      programmingLanguages: ["Javascript", "Python", "C#", "Html/Css"],
+      frameworks: ["Vuejs", "Nodejs", "Expressjs"],
+      other: ["Desktop apps", "Version Control", "MSSQL", "MongoDB"],
       languages: [
         "Turkish(Native)",
         "English(Advanced)",
